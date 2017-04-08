b5b8e9ebe40751dfbb6746882b24e1a931781a3f
Margherita Serino <m.serino@hotmail.it>
2017-04-08 17:04:13 +0100

    Adding sample project

--- app.js
+++ app.js
@@ -2,5 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Components
+import Scoreboard from './components/Scoreboard';
+
 // css
 import './css/style.css';
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
